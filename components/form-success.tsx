fc93ac503c11a7ee8350b0faf89aa38c0beefc8a
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-01-17 00:43:27 +0530

    feat: create schemas and form success or error comp

---
+++ components/form-success.tsx
@@ -0,0 +1,15 @@
+import { CheckCircledIcon } from '@radix-ui/react-icons';
+
+interface FormSuccessProps {
+  message?: string;
+}
+
+export const FormSuccess = ({ message }: FormSuccessProps) => {
+  if (!message) return null;
+  return (
+    <div className="flex items-center gap-x-2 text-sm bg-emerald-500/15 text-emerald-500  p-3">
+      <CheckCircledIcon className="h-4 w-4" />
+      <p>{message}</p>
+    </div>
+  );
+};
