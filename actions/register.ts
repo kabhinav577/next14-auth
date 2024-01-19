83388988a8051d16263cc4aff9a9ea2f43e2ca97
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-01-19 19:21:05 +0530

    feat: register data storing in DB

--- actions/register.ts
+++ actions/register.ts
@@ -1,8 +1,11 @@
 'use server';
 
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
 
+import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -12,6 +15,28 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
       error: 'Invalid Credentials!',
     };
   }
+
+  const { email, name, password } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return {
+      error: 'User already exists!',
+    };
+  }
+
+  const user = await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO:: Send Verification email
 
   return {
     success: 'Email Sent!',
