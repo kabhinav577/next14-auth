9bd2acd0e6f6b18bf6f462e43356bbee50dd6db9
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-01-20 20:11:55 +0530

    feat: protecting routes using middleware.ts and using bcryptjs because bcrypt producing bugs

---
+++ routes.ts
@@ -0,0 +1,28 @@
+/**
+ * An Array of routes that are used for public
+ * these routes do not require authentication
+ * @type {string[]}
+ */
+
+export const publicRoutes = ['/'];
+
+/**
+ * An Array of routes that are used for authentication
+ * these routes will redirect logged in users to /settings
+ * @type {string[]}
+ */
+
+export const authRoutes = ['/auth/login', '/auth/register'];
+
+/**
+ * The Prefix for API Auth Routes
+ * Routes that start with /api/auth with this prefix are used for API Authentication process
+ * @type {string}
+ */
+export const apiAuthPrefix = '/api/auth';
+
+/**
+ * The Default redirect path after login.
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = '/settings';
