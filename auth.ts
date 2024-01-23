5da2d5e882a7d7668c8c7f6dbe185df9381865e1
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-01-23 17:27:17 +0530

    fix: extract sessionToken method in session cb

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from '@/lib/db';
 import authConfig from '@/auth.config';
 import { getUserById } from '@/data/user';
+import { UserRole } from '@prisma/client';
 
 export const {
   handlers: { GET, POST },
@@ -31,7 +32,7 @@ export const {
       }
 
       if (token.role && session.user) {
-        session.user.role = token.role;
+        session.user.role = token.role as UserRole;
       }
 
       return session;
