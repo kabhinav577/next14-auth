bd8cf2c95d32e4b72b8d9aa3c59a629d1250dd76
Krishna Kant <krishnakant@Krishnas-MacBook-Air.local>
2024-01-31 19:37:22 +0530

    feat: protecting Admin routes from user

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+'use client';
+
+import { RoleGate } from '@/components/auth/role-gate';
+import { FormSuccess } from '@/components/form-success';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { UserRole } from '@prisma/client';
+import { toast } from 'sonner';
 
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  const onApiRouteClick = () => {
+    fetch('/api/admin').then((res) => {
+      if (res.ok) {
+        toast.success('Allowed API Routes!');
+      } else {
+        toast.error('Forbidden API Routes!');
+      }
+    });
+  };
+  return (
+    <Card className="w-[600px]">
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">🔑 Admin</p>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          <FormSuccess message="You are an admin" />
+        </RoleGate>
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+          <p className="text-sm font-medium">Admin-Only API Routes</p>
+          <Button onClick={onApiRouteClick}>Click to test!</Button>
+        </div>
+
+        <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
+          <p className="text-sm font-medium">Admin-Only Server Actions</p>
+          <Button>Click to test!</Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AdminPage;
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,12 @@
 import { auth } from '@/auth';
 
+// This Data using on server Component
 export const currentUser = async () => {
   const session = await auth();
   return session?.user;
+};
+
+export const currentRole = async () => {
+  const session = await auth();
+  return session?.user?.role;
 };
